454573f71203f6a4a535b088ad68503d05c305df
Alvmn <asuperalvaro@hotmail.com>
2014-08-14 13:57:34 +0000

    Creates songs with tags

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,18 +11,28 @@ class SongsController < ApplicationController
 		#título parecido al introducido
 	end
 	def new
+		@categories = Category.all
 		@song = @instrument.songs.new
 	end
 
 	def create
 		@song = @instrument.songs.new(title: params[:song_title]) 
 		category = Category.find_by name: params[:song_category]
-		@song.categories << category
+		
+		tag1 = Tag.find_by(name: params[:song_tag1][:Cool]) unless params[:song_tag1][:Cool] == "no"
+		tag2 = Tag.find_by(name: params[:song_tag2][:Long]) unless params[:song_tag2][:Long] == "no"
+		tag3 = Tag.find_by(name: params[:song_tag3][:Weird]) unless params[:song_tag2][:Weird] == "no"
+
+		@song.categories << category if category
+		@song.tags << tag1 if tag1
+		@song.tags << tag2 if tag2
+		@song.tags << tag3 if tag3
 		if @song.save
-			flash[:notice] = "Song succesfully created!"
+			flash[:alert] = "Song succesfully created!"
 			redirect_to action: 'index', controller: 'songs'
 		else
 			flash[:alert] = "Sorry, try again"
+			@categories = Category.all
 			render 'new'
 		end
 

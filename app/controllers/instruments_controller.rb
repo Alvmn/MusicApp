89015360aee372e56584544277cac114c14ab291
HenarDoel <henar_d0el_96@hotmail.es>
2014-08-14 08:24:42 +0000

    form create instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -8,10 +8,27 @@ class InstrumentsController < ApplicationController
 	def show
 		@songs = @instrument.songs
 	end
+	def new
+		@instrument = Instrument.new
+	end
+	def create
+		@instrument = Instrument.new entry_params
+		if @instrument.save 
+			flash[:notice] = 'Your instrument has been created!'
+			redirect_to action: 'index', controller: 'instruments'	
+		else 
+			flash[:notice] = 'Ooohps, your instrument could not be created, try again'
+			render 'new'	
+		end	
+	end 
 
 	protected
 
 	def set_instrument
 	  @instrument = Instrument.find(params[:id])
+	end
+
+	def entry_params
+		params.require(:instrument).permit(:name)
 	end
 end

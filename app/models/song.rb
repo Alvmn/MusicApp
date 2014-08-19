bd92c61b1133dcc4a5fcdb54dde266a85ce42a10
pmurcia <p.murcia.morilla@gmail.com>
2014-08-19 08:17:24 +0000

    Migration of paperclip, not put

--- app/models/song.rb
+++ app/models/song.rb
@@ -16,4 +16,7 @@ class Song < ActiveRecord::Base
 
     extend FriendlyId
   	friendly_id :title, use: :slugged
+
+  	has_attached_file :music_sheets, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  	validates_attachment_content_type :music_sheets, :content_type => /\Aimage\/.*\Z/
 end

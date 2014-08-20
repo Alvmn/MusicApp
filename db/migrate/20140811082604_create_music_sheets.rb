7244d2f8d883ec89a52ef30f89a2135ceb31ec08
Alvmn <asuperalvaro@hotmail.com>
2014-08-20 16:36:05 +0000

    You can add several music_scores. Yeahhhhhhhhhhhhhhh

--- db/migrate/20140811082604_create_music_sheets.rb
+++ db/migrate/20140811082604_create_music_sheets.rb
@@ -2,7 +2,6 @@ class CreateMusicSheets < ActiveRecord::Migration
   def change
     create_table :music_sheets do |t|
 	  t.integer :song_id
-  	  t.string :url
       # t.timestamps
     end
   end

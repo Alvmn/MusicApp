ccb925bd0bbe6db6f7dbe3b8d43179a4fc50d374
pmurcia <p.murcia.morilla@gmail.com>
2014-08-21 18:20:31 +0000

    Add column to user called username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821133016) do
+ActiveRecord::Schema.define(version: 20140821181937) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -80,12 +80,16 @@ ActiveRecord::Schema.define(version: 20140821133016) do
   add_index "roles", ["name"], name: "index_roles_on_name"
 
   create_table "songs", force: true do |t|
-    t.string  "title"
-    t.string  "image"
-    t.integer "instrument_id", limit: 255
-    t.integer "user_id"
-    t.string  "songwriter"
-    t.string  "slug"
+    t.string   "title"
+    t.string   "image"
+    t.integer  "instrument_id",       limit: 255
+    t.integer  "user_id"
+    t.string   "songwriter"
+    t.string   "slug"
+    t.string   "asheet_file_name"
+    t.string   "asheet_content_type"
+    t.integer  "asheet_file_size"
+    t.datetime "asheet_updated_at"
   end
 
   create_table "songs_tags", force: true do |t|
@@ -112,10 +116,12 @@ ActiveRecord::Schema.define(version: 20140821133016) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "users_roles", id: false, force: true do |t|
     t.integer "user_id"

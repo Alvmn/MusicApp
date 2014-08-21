e44d245e3cf547cb12704d7f78474426100b7dab
Alvmn <asuperalvaro@hotmail.com>
2014-08-21 13:53:20 +0000

    Upload audio workssssssss

--- app/models/midi.rb
+++ app/models/midi.rb
@@ -1,6 +1,10 @@
 class Midi < ActiveRecord::Base
 	belongs_to :song
 
-	validates :url, uniqueness: true
-	validates :url, presence: true
+	has_attached_file :audio_file,
+	  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+	  :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension"
+  validates_attachment :audio_file,
+ 	 :content_type => { :content_type => [ 'application/mp3', 'application/x-mp3', 'application/force- 
+download', 'audio/mpeg', 'audio/mp3' ]}
 end
